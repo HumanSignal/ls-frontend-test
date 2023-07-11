8d54a5a91051a5de25cfafafa2c92de9cc9a4d35
Brandon Martel <brandonmartel@gmail.com>
2023-07-10 23:21:02 -0500

    add audio playback helpers

--- helpers/LSF/AudioView.ts
+++ helpers/LSF/AudioView.ts
@@ -1,38 +1,63 @@
-import TriggerOptions = Cypress.TriggerOptions
-import ObjectLike = Cypress.ObjectLike
-import ClickOptions = Cypress.ClickOptions
+import TriggerOptions = Cypress.TriggerOptions;
+import ObjectLike = Cypress.ObjectLike;
+import ClickOptions = Cypress.ClickOptions;
 
-type MouseInteractionOptions = Partial<TriggerOptions & ObjectLike & MouseEvent>
+type MouseInteractionOptions = Partial<
+  TriggerOptions & ObjectLike & MouseEvent
+>;
 
 export const AudioView = {
   get root() {
-    return cy
-      .get('.lsf-audio-tag');
+    return cy.get(".lsf-audio-tag");
   },
   get drawingArea() {
-    return this
-      .root
-      .get('canvas');
+    return this.root.get("canvas");
   },
   get timelineControls() {
-    return this.root
-      .get('.lsf-timeline-controls');
+    return this.root.get(".lsf-timeline-controls");
+  },
+  get currentTimebox() {
+    return cy.get('[data-testid="timebox-current-time"] > .lsf-time-box__input-time')
+  },
+  get endTimebox() {
+    return cy.get('[data-testid="timebox-end-time"] > .lsf-time-box__input-time')
   },
   get configButton() {
-    return this.timelineControls
-      .get('.lsf-audio-config > .lsf-button')
+    return this.timelineControls.get(".lsf-audio-config > .lsf-button");
   },
   get volumeButton() {
-    return this.timelineControls
-      .get('.lsf-audio-control > .lsf-button')
+    return this.timelineControls.get(".lsf-audio-control > .lsf-button");
   },
   get loadingBar() {
-    return this.root
-      .get('loading-progress-bar', { timeout: 10000 });
+    return this.root.get("loading-progress-bar", { timeout: 10000 });
   },
   isReady() {
-    this.loadingBar
-      .should('not.exist');
+    this.loadingBar.should("not.exist");
+  },
+  get playButton() {
+    return cy.get(`[data-testid="playback-button:play"]`);
+  },
+  get pauseButton() {
+    return cy.get(`[data-testid="playback-button:pause"]`);
+  },
+  seekCurrentTimebox(to: number) {
+    let timeString = ''
+    timeString = `0000${to}000`;
+
+    this.currentTimebox.click({force: true }).clear().type(timeString, { force: true}).blur();
+  },
+  pause() {
+    this.pauseButton.click();
+  },
+  play(from?: number, to?: number) {
+    if (from) {
+      this.seekCurrentTimebox(from);
+    }
+    this.playButton.click();
+    if (to) {
+      cy.wait((to - (from || 0)) * 1000);
+      this.pause();
+    }
   },
   /**
    * Clicks at the coordinates on the drawing area
@@ -41,9 +66,7 @@ export const AudioView = {
    */
   clickAt(x: number, y: number, options?: Partial<ClickOptions>) {
     cy.log(`Click at the AudioView at (${x}, ${y})`);
-    this.drawingArea
-      .scrollIntoView()
-      .click(x, y, options);
+    this.drawingArea.scrollIntoView().click(x, y, options);
   },
   /**
    * Clicks at the relative coordinates on the drawing area
@@ -67,13 +90,31 @@ export const AudioView = {
    * @param {number} width
    * @param {number} height
    */
-  drawRect(x: number, y: number, width: number, height: number, options: MouseInteractionOptions = {}) {
+  drawRect(
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    options: MouseInteractionOptions = {}
+  ) {
     cy.log(`Draw rectangle at (${x}, ${y}) of size ${width}x${height}`);
     this.drawingArea
       .scrollIntoView()
-      .trigger('mousedown', x, y, { eventConstructor: 'MouseEvent', buttons: 1, ...options })
-      .trigger('mousemove', x + width, y + height, { eventConstructor: 'MouseEvent', buttons: 1, ...options })
-      .trigger('mouseup', x + width, y + height, { eventConstructor: 'MouseEvent', buttons: 1, ...options })
+      .trigger("mousedown", x, y, {
+        eventConstructor: "MouseEvent",
+        buttons: 1,
+        ...options
+      })
+      .trigger("mousemove", x + width, y + height, {
+        eventConstructor: "MouseEvent",
+        buttons: 1,
+        ...options
+      })
+      .trigger("mouseup", x + width, y + height, {
+        eventConstructor: "MouseEvent",
+        buttons: 1,
+        ...options
+      })
       .wait(0);
   },
   /**
@@ -84,7 +125,13 @@ export const AudioView = {
    * @param {number} width
    * @param {number} height
    */
-  drawRectRelative(x: number, y: number, width: number, height: number, options: MouseInteractionOptions = {}) {
+  drawRectRelative(
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    options: MouseInteractionOptions = {}
+  ) {
     this.drawingArea.then(el => {
       const bbox: DOMRect = el[0].getBoundingClientRect();
       const realX = x * bbox.width;
@@ -97,13 +144,13 @@ export const AudioView = {
   },
 
   /**
-  * Matches the visual appearance of the entire AudioView component
-  */
-  toMatchImageSnapshot(el?: Cypress.Chainable<JQuery<HTMLElement>>, options?: { threshold?: number }) {
+   * Matches the visual appearance of the entire AudioView component
+   */
+  toMatchImageSnapshot(
+    el?: Cypress.Chainable<JQuery<HTMLElement>>,
+    options?: { threshold?: number }
+  ) {
     el = el || this.root;
-    el
-      .wait(0)
-      .matchImageSnapshot(options);
-  },
+    el.wait(0).matchImageSnapshot(options);
+  }
 };
-

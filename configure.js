18575bf951e640f0448fb85b1eacb105ebd98c9e
Sergey <sergey.koshevarov@heartex.com>
2023-07-12 08:48:29 +0100

    Fix coverage collecting in parallel mode

--- configure.js
+++ configure.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'cypress';
 import path from 'path';
 import { setupTypescript } from './plugins/typescript';
-import installLogsPrinter from "cypress-terminal-report/src/installLogsPrinter";
+import installLogsPrinter from 'cypress-terminal-report/src/installLogsPrinter';
 import * as tasks from './tasks';
 import { disableChromeGPU } from './plugins/disable_gpu';
-import cypressCoverageTask from '@cypress/code-coverage/task';
+import { coverageParallel } from './plugins/coverage_parallel.js';
 
 const LSF_PORT = process.env.LSF_PORT ?? '3000';
 const COLLECT_COVERAGE = process.env.COLLECT_COVERAGE === 'true' || process.env.COLLECT_COVERAGE === '1';
@@ -37,11 +37,11 @@ export default function(configModifier, setupNodeEvents) {
       // output config
       setupNodeEvents(on, config) {
         // Allows collecting coverage
-        cypressCoverageTask(on, config);
+        coverageParallel(on, config);
         on('task', { ...tasks });
         // Gives a step-by-step output for failed tests in headless mode
         installLogsPrinter(on, {
-          outputVerbose: false
+          outputVerbose: false,
         });
         // Allows compiling TS files from node_modules (this package)
         setupTypescript(on, config);

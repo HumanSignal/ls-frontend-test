2c9410fb930c7d920474fe6e624efee6261cec0c
Nick Skriabin <nr@fenelon.ru>
2024-01-10 14:20:43 +0000

    fix hotkeys for win/mac

--- helpers/LSF/Hotkeys.ts
+++ helpers/LSF/Hotkeys.ts
@@ -1,11 +1,13 @@
 function pressHotkey(hotkey: string) {
-  const isMacos = cy.window().then($win => {
-    return $win.navigator.userAgent.match(/macos/) !== null;
-  });
+  cy.window()
+    .then($win => {
+      return $win.navigator.userAgent.match(/macintosh/i) !== null;
+    })
+    .then(isMacos => {
+      const hotkeyString = isMacos ? hotkey.replace(/ctrl/g, "cmd") : hotkey;
 
-  const hotkeyString = isMacos ? hotkey.replace(/ctrl/g, "cmd") : hotkey;
-
-  cy.get("body").type(hotkeyString);
+      cy.get("body").type(hotkeyString);
+    });
 }
 
 export const Hotkeys = {
